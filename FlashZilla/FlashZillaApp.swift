9d15967fee5cc680f2ca2e139a6684805099ed59
sxmtee <somtoo76@gmail.com>
2024-11-26 17:35:21 +0100

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by mac on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
